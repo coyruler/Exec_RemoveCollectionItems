e2fd210054a3945d9ee4f68ee66b50f1ef52e633
Tony <tunglin.yang1@gmail.com>
2022-10-24 19:32:55 +0800

    加入專案檔案。

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{/// 在集合中移除多個項目
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // 在集合中移除多個項目
+            int[] source = { 3, 5, 7, 9, 11, 13 };
+            int[] target = { 7, 11, 99 };
+            // TODO result應該是{3,5,9,13}
+            var result = source.Except(target).ToList();
+            
+            string list = string.Join(", ", result);
+            string msg = $"{{ 3,5,7,9,11,13}} 移除 {{ 7, 11, 99 }} 的結果為 {list}";
+            Console.WriteLine(msg);
+        }
+    }
+}
